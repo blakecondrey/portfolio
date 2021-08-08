8ada68b8eef8d600025254e07fb1963e70b9a57c
blakecondrey <79805333+blakecondrey@users.noreply.github.com>
2021-08-07 22:30:02 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+PKG_NAME = "website"
+setup(name = PKG_NAME,
+        version = "1.0",
+        url='https://blakecondrey.herokuapp.com',
+        packages = [PKG_NAME])
