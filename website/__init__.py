5230eacf062dc254d5ffede92937996c37721825
blakecondrey <79805333+blakecondrey@users.noreply.github.com>
2021-08-10 10:21:05 -0600

    removed unnecessary info

--- website/__init__.py
+++ website/__init__.py
@@ -1,22 +1,16 @@
 from flask import Flask,app
 from flask_mail import Mail, Message
 
-from .form import ContactForm, csrf
 
 def create_app():
 
     from .routing import routing
 
-    # mail = Mail()
     app = Flask(__name__)
     app.register_blueprint(routing, url_prefix='/')
     app.config["SECRET_KEY"] = "iNeedCoffeeAlways1!"
     app.config['MAIL_SERVER']='smtp.gmail.com'
     app.config['MAIL_PORT'] = 465
-    app.config['MAIL_USERNAME'] = 'condrey.blake1217@gmail.com'
-    app.config['MAIL_PASSWORD'] = 'Valeria08!'
     app.config['MAIL_USE_SSL'] = True
-    # csrf.init_app(app)
-    # mail.init_app(app)
 
     return app
